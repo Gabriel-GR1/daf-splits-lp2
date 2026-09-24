b4c3c504c22a72e820e8cad8fbf9483a9ddb91b0
Gabriel Rodrigo <93385461+Gabriel-GR1@users.noreply.github.com>
2026-09-23 21:47:52 -0300

    Replace Meta Pixel with TikTok Pixel on LP2 and track Lead on group CTA

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-// Meta Pixel
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('set', 'autoConfig', false, '1470839471393001');
-fbq('init', '1470839471393001');
-fbq('track', 'PageView');
+// TikTok Pixel (LP2)
+!function (w, d, t) {
+  w.TiktokAnalyticsObject = t;
+  var ttq = w[t] = w[t] || [];
+  ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie", "holdConsent", "revokeConsent", "grantConsent"];
+  ttq.setAndDefer = function (queue, method) {
+    queue[method] = function () {
+      queue.push([method].concat(Array.prototype.slice.call(arguments, 0)));
+    };
+  };
+  for (var i = 0; i < ttq.methods.length; i++) {
+    ttq.setAndDefer(ttq, ttq.methods[i]);
+  }
+  ttq.instance = function (id) {
+    for (var queue = ttq._i[id] || [], i = 0; i < ttq.methods.length; i++) {
+      ttq.setAndDefer(queue, ttq.methods[i]);
+    }
+    return queue;
+  };
+  ttq.load = function (id, options) {
+    var url = "https://analytics.tiktok.com/i18n/pixel/events.js";
+    ttq._i = ttq._i || {};
+    ttq._i[id] = [];
+    ttq._i[id]._u = url;
+    ttq._t = ttq._t || {};
+    ttq._t[id] = +new Date();
+    ttq._o = ttq._o || {};
+    ttq._o[id] = options || {};
+    var script = d.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src = url + "?sdkid=" + id + "&lib=" + t;
+    var firstScript = d.getElementsByTagName("script")[0];
+    firstScript.parentNode.insertBefore(script, firstScript);
+  };
+  ttq.load("DAQ2BHBC77UFPT802QG0");
+  ttq.page();
+}(window, document, "ttq");
 
 const WHATSAPP_GROUP_URL = 'https://chat.whatsapp.com/Kb1fSaWOySr3M9aRyGnULk?s=cl&p=i&mlu=0&ilr=4';
 
@@ -19,8 +45,9 @@ document.querySelectorAll('[data-whatsapp]').forEach((link) => {
   link.rel = 'noopener noreferrer';
 
   link.addEventListener('click', () => {
-    if (typeof window.fbq === 'function') {
-      window.fbq('track', 'Lead');
+    // Mantém a conversão principal da LP2 como Lead, agora no TikTok.
+    if (typeof window.ttq?.track === 'function') {
+      window.ttq.track('Lead');
     }
 
     if (Array.isArray(window.dataLayer)) {
